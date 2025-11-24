d35028720d9e9193234bdf7cf474b60f35199683
JNTMTMTM <15251865468@163.com>
2025-11-24 21:42:57 +0800

    V1.0.0 添加__all__导出列表并创建README文档

--- SHICTHRSWindowsDefenderManager/SHICTHRSWindowsDefenderManager/__init__.py
+++ SHICTHRSWindowsDefenderManager/SHICTHRSWindowsDefenderManager/__init__.py
@@ -17,6 +17,8 @@ print('\033[1mWelcome to use SHRWindowsDefenderManager - enable/disable Windows 
 print('|  \033[1mAlgorithms = rule ; Questioning = approval\033[0m')
 print('|  \033[1mCopyright : © 2025-2026 SHICTHRS, Std. All rights reserved.\033[0m\n')
 
+__all__ = ['SHRWindowsDefenderManager_check_defender' , 'SHRWindowsDefenderManager_enable_defender' , 'SHRWindowsDefenderManager_disable_defender']
+
 class SHRWindowsDefenderManagerException(BaseException):
     def __init__(self , message: str) -> None:
         self.message = message
